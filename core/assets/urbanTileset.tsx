d919e75501543e07c496097999389d6008e4017d
Danya <danilmakarneko0105@gmail.com>
2020-06-03 14:57:29 +0300

    Ocean Level (4)

---
+++ core/assets/urbanTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="urban" tilewidth="32" tileheight="32" tilecount="5250" columns="70">
+ <image source="../../../../../urban.png" width="2240" height="2400"/>
+</tileset>
